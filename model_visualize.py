210c679245874090f3d9df1169719724c5789c3a
SleepyPanda <98614364+DreamyStranger@users.noreply.github.com>
2025-04-22 01:29:02 -0400

    config fixes

--- model_visualize.py
+++ model_visualize.py
@@ -3,11 +3,11 @@ import highway_env
 from stable_baselines3 import DQN
 from utils.utils import get_user_choices, load_env_config, build_model_path
 
-env_id, mode = get_user_choices()
-env_config = load_env_config()
+env_id, mode, observation_type, run_id = get_user_choices()
+env_config = load_env_config(observation_type)
 env = gym.make(env_id, render_mode="rgb_array", config=env_config)
 folder_name = "models"
-model_save_path = build_model_path(folder_name, env_id, mode)
+model_save_path = build_model_path(folder_name, env_id, mode, observation_type, run_id)
 
 model = DQN.load(model_save_path)
 while True:
